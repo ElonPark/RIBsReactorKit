4dc2ed5dbcde08fcd560b07153f2c8d49c9351cb
Elon Park <sungwoon.park92@gmail.com>
2020-05-02 02:30:01 +0900

    :sparkles:  Add RandomUser API

--- RIBsReactorKit/RIBsReactorKit/Source/Domain/UseCases/RandomUserUseCase.swift
+++ RIBsReactorKit/RIBsReactorKit/Source/Domain/UseCases/RandomUserUseCase.swift
@@ -10,20 +10,67 @@ import RxSwift
 import RxCocoa
 
 protocol RandomUserUseCase {
+  var requestItemCount: Int { get }
+  var isLastItems: Bool { get }
+  var provider: UserModelProvider { get }
+  var repository: RandomUserRepository { get }
   var userModels: BehaviorRelay<[UserModel]> { get }
-  func loadData() -> Observable<Void>
+
+  func loadData(isRefresh: Bool) -> Observable<Void>
 }
 
 final class RandomUserUseCaseImpl: RandomUserUseCase {
   
   // MARK: - Properties
   
+  let requestItemCount: Int
+  
+  private (set) var isLastItems: Bool = false
+  
+  let repository: RandomUserRepository
+  
+  let provider: UserModelProvider
+  
   let userModels: BehaviorRelay<[UserModel]> = .init(value: [])
+  
+  // MARK: - Con(De)structor
+
+  init(requestItemCount: Int, repository: RandomUserRepository, provider: UserModelProvider) {
+    self.requestItemCount = requestItemCount
+    self.repository = repository
+    self.provider = provider
+  }
   
   // MARK: - Internal methods
   
-  func loadData() -> Observable<Void> {
-    // - TODO: 구현 2020-04-27 01:40:07
-    return .just(Void())
+  func loadData(isRefresh: Bool) -> Observable<Void> {
+    let randomUsers: Single<RandomUser>
+    if let info = repository.info, !isRefresh {
+      let page = info.page + 1
+      randomUsers = repository.randomUsers(with: page, count: requestItemCount, seed: info.seed)
+    } else {
+      randomUsers = repository.randomUsers(with: requestItemCount)
+    }
+    
+    return randomUsers
+      .asObservable()
+      .map { $0.results }
+      .do(onNext: { [weak self] results in
+        self?.setIsLastItems(by: results)
+        self?.updateUserModels(by: results)
+      })
+      .map { _ in Void() }
+  }
+  
+  // MARK: - Private methods
+  
+  private func setIsLastItems(by results: [User]) {
+    isLastItems = results.isEmpty || results.count < requestItemCount
+  }
+  
+  private func updateUserModels(by results: [User]) {
+    var models = userModels.value
+    models.append(contentsOf: provider.makeUserModel(by: results))
+    userModels.accept(models)
   }
 }
---
+++ RIBsReactorKit/RIBsReactorKit/Source/Platform/DataSource/Networking.swift
@@ -0,0 +1,129 @@
+//
+//  Networking.swift
+//  RIBsReactorKit
+//
+//  Created by Elon on 2020/04/25.
+//  Copyright © 2020 Elon. All rights reserved.
+//
+
+import Alamofire
+import Moya
+import RxMoya
+import RxSwift
+
+final class Networking<Target: TargetType>: MoyaProvider<Target> {
+  
+  func request(
+    _ target: Target,
+    file: String = #file,
+    function: String = #function,
+    line: UInt = #line
+  ) -> Single<Response> {
+    let requestString = "\(target.method) \(target.path)"
+    
+    return self.rx.request(target)
+      .filterSuccessfulStatusCodes()
+      .do(onSuccess: { response in
+        let message = "SUCCESS: \(requestString) (\(response.description))"
+        Log.debug(fileName: file, line: line, funcName: function, message)
+      }, onError: { [weak self] error in
+        var message = "FAILURE: \(requestString)"
+        guard let this = self else {
+          Log.warning(
+            fileName: file,
+            line: line,
+            funcName: function,
+            message,
+            error.localizedDescription,
+            error
+          )
+          return
+        }
+        
+        let afError = this.loggingAFError(message: message, error: error)
+        message = afError.message
+        
+        let isLoggingMoyaError = this.isLoggingMoyaError(
+          file: file,
+          function: function,
+          line: line,
+          hasStatusCode: afError.hasStatusCode,
+          message: message,
+          error: error
+        )
+        
+        if !isLoggingMoyaError {
+          Log.warning(
+            fileName: file,
+            line: line,
+            funcName: function,
+            message,
+            error.localizedDescription,
+            error
+          )
+        }
+        }, onSubscribed: {
+          let message = "REQUEST: \(requestString)"
+          Log.debug(fileName: file, line: line, funcName: function, message)
+      })
+  }
+  
+  private func loggingAFError(
+    message: String,
+    error: Error
+  ) -> (message: String, hasStatusCode: Bool)  {
+    var hasStatusCode: Bool = false
+    guard let afError = error.asAFError else { return (message, hasStatusCode) }
+    var message = message
+    
+    if let responseCode = afError.responseCode {
+      message += " Status Code: \(responseCode)"
+      hasStatusCode = true
+    }
+    if let failureReason = afError.failureReason {
+      message += "\nFailure Reason: \(failureReason)"
+    }
+    if let recoverySuggestion = afError.recoverySuggestion {
+      message += "\nRecovery Suggestion: \(recoverySuggestion)"
+    }
+    if let contentType = afError.responseContentType {
+      message += "\nContent Type: \(contentType)"
+    }
+    if let errorDescription = afError.errorDescription {
+      message += "\nDescription: \(errorDescription)"
+    }
+    
+    message += "\n\(afError.localizedDescription)"
+    
+    return (message, hasStatusCode)
+  }
+  
+  private func isLoggingMoyaError(
+    file: String,
+    function: String,
+    line: UInt,
+    hasStatusCode: Bool,
+    message: String,
+    error: Error
+  ) -> Bool {
+    guard let moyaError = error as? MoyaError,
+      let response = moyaError.response else {
+        return false
+    }
+    
+    var message = message
+    
+    if !hasStatusCode {
+      message += " \(response.description)"
+    }
+    
+    if let jsonObject = try? response.mapJSON(failsOnEmptyData: false) {
+      Log.warning(fileName: file, line: line, funcName: function, message, jsonObject)
+    } else {
+      let rawString = String(data: response.data, encoding: .utf8) ?? ""
+      Log.warning(fileName: file, line: line, funcName: function, message, rawString)
+    }
+    
+    return true
+  }
+}
