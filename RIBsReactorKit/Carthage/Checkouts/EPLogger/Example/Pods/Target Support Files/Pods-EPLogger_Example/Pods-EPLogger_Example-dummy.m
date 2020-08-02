b1e90ef22266cfc00ddc766b733e44705e92dc45
Elon Park <sungwoon.park92@gmail.com>
2020-08-03 04:32:17 +0900

    :wrench: Chore gitignore

--- RIBsReactorKit/Carthage/Checkouts/EPLogger/Example/Pods/Target Support Files/Pods-EPLogger_Example/Pods-EPLogger_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EPLogger_Example : NSObject
-@end
-@implementation PodsDummy_Pods_EPLogger_Example
-@end
--- RIBsReactorKit/Carthage/Checkouts/Moya/Gemfile
+++
@@ -1,9 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'xcpretty'
-
-gem 'cocoapods', '~> 1.6'
-gem 'rake'
-gem 'octokit', '~> 4.3'
-
-gem 'xcpretty-json-formatter'
