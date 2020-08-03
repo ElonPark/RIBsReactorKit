9bc4ec7e5d26b68535fab220ddfb7b3f4845b2b7
Sungwoon Park <sungwoon.park92@gmail.com>
2020-08-04 03:17:03 +0900

    :bug: Fix install.sh

--- RIBsReactorKit/install.sh
+++ RIBsReactorKit/install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-# brew bundle
+brew bundle
 
-# carthage bootstrap --platform iOS --no-build
+carthage bootstrap --platform iOS --no-build
 
-# (cd Carthage/Checkouts/WeakMapTable && swift package generate-xcodeproj)
-# (cd Carthage/Checkouts/ReactorKit && swift package generate-xcodeproj)
+(cd Carthage/Checkouts/WeakMapTable && swift package generate-xcodeproj)
+(cd Carthage/Checkouts/ReactorKit && swift package generate-xcodeproj)
 
-# carthage build --platform iOS
-# carthage update --platform iOS --no-use-binaries
+carthage build --platform iOS
+carthage update --platform iOS --no-use-binaries
 
 if which yarn >/dev/null; then
   (cd ./RIBsTreeViewer/Browser && npx yarn install)
