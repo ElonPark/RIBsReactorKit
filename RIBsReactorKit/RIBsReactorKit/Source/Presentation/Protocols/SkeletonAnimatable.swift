1cd4788b00fbacb2e552a29502dedb1577df0b43
Elon Park <sungwoon.park92@gmail.com>
2020-08-05 00:20:57 +0900

    :sparkles: Add SkeletonAnimatable

---
+++ RIBsReactorKit/RIBsReactorKit/Source/Presentation/Protocols/SkeletonAnimatable.swift
@@ -0,0 +1,30 @@
+//
+//  SkeletonAnimatable.swift
+//  RIBsReactorKit
+//
+//  Created by Elon on 2020/08/04.
+//  Copyright © 2020 Elon. All rights reserved.
+//
+
+import UIKit
+
+import SkeletonView
+
+protocol SkeletonAnimatable {
+  var views: [UIView] { get }
+  
+  func showSkeletonAnimation()
+  func hideSkeletonAnimation()
+}
+
+extension SkeletonAnimatable {
+  func showSkeletonAnimation() {
+    views.forEach { $0.showAnimatedGradientSkeleton() }
+  }
+  
+  func hideSkeletonAnimation() {
+    views
+      .filter(\.isSkeletonActive)
+      .forEach { $0.hideSkeleton(transition: .crossDissolve(0.25)) }
+  }
+}
