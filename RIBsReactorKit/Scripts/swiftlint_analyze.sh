338f67f2e43f9809b99c748a82d7c5a90130b182
Elon Park <sungwoon.park92@gmail.com>
2020-08-17 23:01:29 +0900

    :hammer: Update swiftlint_analyze.sh

--- RIBsReactorKit/Scripts/swiftlint_analyze.sh
+++ RIBsReactorKit/Scripts/swiftlint_analyze.sh
@@ -1,3 +1,4 @@
+rm -rf ~/Library/Developer/Xcode/DerivedData/*
 xcodebuild -scheme RIBsReactorKit > xcodebuild.log
 swiftlint analyze --compiler-log-path xcodebuild.log > result.html
 open result.html
