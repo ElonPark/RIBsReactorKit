73d52efaca95934404884adef9e48b81517afc76
Elon Park <dev.sungwoon@gmail.com>
2020-12-20 05:23:14 +0900

    :wrench: Fix xcode12 build

--- RIBsReactorKit/install.sh
+++ RIBsReactorKit/install.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-brew bundle
+# brew bundle
 
-carthage bootstrap --platform iOS --new-resolver --no-use-binaries --cache-builds
+sh carthage_build.sh bootstrap --platform iOS --new-resolver --no-use-binaries
 
 rm -rf ./Carthage/Checkouts/ReactiveSwift
 rm -rf ./Carthage/Build/iOS/ReactiveSwift.*
 rm -rf ./Carthage/Build/iOS/ReactiveMoya.*
 
-if which yarn >/dev/null; then
-  (cd ./RIBsTreeViewer/Browser && npx yarn install)
-  (cd ./RIBsTreeViewer/Browser && npx webpack)
-  (cd ./RIBsTreeViewer/WebSocketServer && npx yarn install)
-else
-  echo "warning: yarn not installed, try 'brew install yarn'"
-fi
+# if which yarn >/dev/null; then
+#   (cd ./RIBsTreeViewer/Browser && npx yarn install)
+#   (cd ./RIBsTreeViewer/Browser && npx webpack)
+#   (cd ./RIBsTreeViewer/WebSocketServer && npx yarn install)
+# else
+#   echo "warning: yarn not installed, try 'brew install yarn'"
+# fi
