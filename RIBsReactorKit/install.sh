9541a44ea783d9302ddab54bb40a242eb1445242
Elon Park <sungwoon.park92@gmail.com>
2020-12-21 01:59:17 +0900

    :wrench: Fix install.sh

--- RIBsReactorKit/install.sh
+++ RIBsReactorKit/install.sh
@@ -8,10 +8,10 @@ rm -rf ./Carthage/Checkouts/ReactiveSwift
 rm -rf ./Carthage/Build/iOS/ReactiveSwift.*
 rm -rf ./Carthage/Build/iOS/ReactiveMoya.*
 
-# if which yarn >/dev/null; then
-#   (cd ./RIBsTreeViewer/Browser && npx yarn install)
-#   (cd ./RIBsTreeViewer/Browser && npx webpack)
-#   (cd ./RIBsTreeViewer/WebSocketServer && npx yarn install)
-# else
-#   echo "warning: yarn not installed, try 'brew install yarn'"
-# fi
+if which yarn >/dev/null; then
+  (cd ./RIBsTreeViewer/Browser && npx yarn install)
+  (cd ./RIBsTreeViewer/Browser && npx webpack)
+  (cd ./RIBsTreeViewer/WebSocketServer && npx yarn install)
+else
+  echo "warning: yarn not installed, try 'brew install yarn'"
+fi
