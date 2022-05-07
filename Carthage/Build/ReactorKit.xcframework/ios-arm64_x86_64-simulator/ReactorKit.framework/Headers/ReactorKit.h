273d53fc8743036ce9a1df93e8d96395a3ec94d4
Elon Park <dev.sungwoon@gmail.com>
2022-05-08 02:11:41 +0900

    👷ci: add xcframeworks

---
+++ Carthage/Build/ReactorKit.xcframework/ios-arm64_x86_64-simulator/ReactorKit.framework/Headers/ReactorKit.h
@@ -0,0 +1,17 @@
+//
+//  ReactorKit.h
+//  ReactorKit
+//
+//  Created by Elon on 2020/12/20.
+//  Copyright © 2020 ReactorKit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReactorKit.
+FOUNDATION_EXPORT double ReactorKitVersionNumber;
+
+//! Project version string for ReactorKit.
+FOUNDATION_EXPORT const unsigned char ReactorKitVersionString[];
+
+#import "ReactorKitRuntime.h"
