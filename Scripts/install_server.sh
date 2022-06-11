e36ec4a21c5051a0d579985be376f0b58c184e69
Elon Park <dev.sungwoon@gmail.com>
2022-06-12 00:37:16 +0900

    :poop: clean: remove RIBsTreeViewer

--- Scripts/install_server.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if which yarn >/dev/null; then
-  (cd ../RIBsTreeViewer/Browser && npx yarn install && npx webpack)
-  (cd ../RIBsTreeViewer/WebSocketServer && npx yarn install)
-else
-  echo "warning: yarn not installed, try 'brew install yarn'"
-fi
